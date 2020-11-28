88057bf8a73d586a3178256f1441314a901ba17d
Abid Al-Wasi <abidalwasi@outlook.com>
2020-11-28 15:39:13 +0600

    added other files excluding node modules

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Run when client connects
+io.on("connection", socket => {
+  console.log("New WS Connection...")
+})
+
+
+const PORT = 3000 || process.env.PORT;
+// const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
