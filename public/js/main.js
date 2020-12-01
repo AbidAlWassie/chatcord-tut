a34ae3555b066a4204d394d41670daf736e2b0f9
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-01 23:54:51 +0600

    added everything

--- public/js/main.js
+++ public/js/main.js
@@ -60,9 +60,8 @@ function outputRoomName(room) {
 }
 
 // Add users to DOM
-
 function outputUsers(users) {
   userList.innerHTML = `
-  ${users.map(user => `<li>${user.username}</li>`).join()}
+  ${users.map(user => `<li>${user.username}</li>`).join("")}
   `;
 }
